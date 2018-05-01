88be971a1be8279ad8ae27cbe138c4d7a70a249b
Karl Phillip <karlphillip@gmail.com>
2018-05-01 01:13:52 -0300

    Capstone Project1: updated data loading procedure in clean_db according to data_wrangling_v3

--- Capstone Project1/lib/clean_db.py
+++ Capstone Project1/lib/clean_db.py
@@ -7,7 +7,7 @@ import time
 with_tqdm = True
 try:
     from tqdm import tqdm_notebook as tqdm
-    pbar = tqdm(total = 12)
+    pbar = tqdm(total = 9)
 except ImportError:
     with_tqdm = False    
 
@@ -41,64 +41,69 @@ def load_from(pwd):
         pbar.update(1) 
 
     ###
-    ### 3.1  df = air_visit_df + air_reserve_df
+    ### 3.1  air_df = air_visit_df + air_reserve_df
     ###
 
     # Add a visit_date column for air_reserve
     air_reserve_df['visit_datetime'] = pd.to_datetime(air_reserve_df['visit_datetime'])
     air_reserve_df['visit_date'] = air_reserve_df['visit_datetime'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
-    # Rename columns so they match during merge
-    air_reserve_df.rename(columns={'reserve_visitors': 'visitors'}, inplace=True)
-    df = pd.merge(air_visit_df, air_reserve_df, on=["air_store_id", "visit_date", "visitors"], how="outer")
+    reserve = air_reserve_df[["air_store_id","visit_date","reserve_visitors"]].groupby(["air_store_id","visit_date"], as_index = False).sum()
+    reserve.rename(columns = {'reserve_visitors': 'reservations'}, inplace = True)
+    air_df = pd.merge(air_visit_df, reserve, on = ['visit_date','air_store_id'], how = "left")
+      
+    if (with_tqdm): 
+        pbar.update(1) 
+
+    ###
+    ### 3.2  air_df = air_df + air_store_df
+    ###
+
+    air_df = pd.merge(air_df, air_store_df, on="air_store_id", how="inner")
     
     if (with_tqdm): 
         pbar.update(1) 
 
     ###
-    ### 3.2  df = df + air_store_df
+    ### 3.3  hpg_df = hpg_reserve_df + hpg_store_df
     ###
 
-    df = pd.merge(df, air_store_df, on="air_store_id", how="left")
-    
+    hpg_df = pd.merge(hpg_reserve_df, hpg_store_df, on="hpg_store_id", how="inner")
+
     if (with_tqdm): 
         pbar.update(1) 
 
     ###
-    ### 3.3  hpg_df = hpg_reserve_df + store_ids_df
+    ### 3.4 hpg_df = hpg_df + store_ids_df
     ###
 
     # convert visit_datetime from string to datetime object to add a new column containing only the Y-M-D
-    hpg_reserve_df['visit_datetime'] = pd.to_datetime(hpg_reserve_df['visit_datetime'])
-    hpg_reserve_df['visit_date'] = hpg_reserve_df['visit_datetime'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    hpg_df['visit_datetime'] = pd.to_datetime(hpg_df['visit_datetime'])
+    hpg_df['visit_date'] = hpg_df['visit_datetime'].apply(lambda x: x.strftime('%Y-%m-%d'))
 
-    # replace the HPG code in hpg_merged_df for its equivalent AIR code
-    hpg_df = pd.merge(hpg_reserve_df, store_ids_df, on='hpg_store_id', how='left')
+    hpg_df = pd.merge(hpg_df, store_ids_df, on='hpg_store_id', how='inner')
     
     if (with_tqdm): 
         pbar.update(1) 
 
     ###
-    ### 3.4 hpg_df = hpg_df + hpg_store_df
-    ###
-
-    hpg_df = pd.merge(hpg_df, hpg_store_df, on="hpg_store_id", how="left")
-    
-    if (with_tqdm): 
-        pbar.update(1) 
-
-    ###
-    ### 3.5 df = df + hpg_df
+    ### 3.5 df = air_df + hpg_df
     ###
 
     # rename columns in both dataframes so they blend in nicely during merge
-    hpg_df.rename(columns={'reserve_visitors': 'visitors'}, inplace=True)
+    hpg_df.rename(columns={'reserve_visitors': 'reservations'}, inplace=True)
     hpg_df.rename(columns={'hpg_genre_name': 'genre_name'}, inplace=True)
     hpg_df.rename(columns={'hpg_area_name' : 'area_name'}, inplace=True)
-    df.rename(columns={'air_genre_name': 'genre_name'}, inplace=True)
-    df.rename(columns={'air_area_name' : 'area_name'}, inplace=True)
+    hpg_df.drop('hpg_store_id',  axis=1, inplace=True)
 
-    df = pd.concat([df, hpg_df], axis=0).reset_index(drop=True)
+    air_df.rename(columns={'air_genre_name': 'genre_name'}, inplace=True)
+    air_df.rename(columns={'air_area_name' : 'area_name'}, inplace=True)
+
+    hpg_cols = ['air_store_id', 'visit_date', 'reservations']
+    df = pd.merge(air_df, hpg_df[hpg_cols], on=hpg_cols[:-1], how='left')
+
+    df['reservations'] = df['reservations_x'] + df['reservations_y']
+    df.drop(['reservations_x', 'reservations_y'], 1, inplace=True)
     
     if (with_tqdm): 
         pbar.update(1) 
@@ -107,11 +112,8 @@ def load_from(pwd):
     ### 3.6 df = df + date_info_df
     ###
 
-    # convert column visit_datetime from string to datetime object
-    df['visit_datetime'] = pd.to_datetime(df['visit_datetime'])
-
-    # combine both datasets to add columns 'day_of_week' and 'holiday_flg' to merged_df
-    df = pd.merge(df, date_info_df, left_on='visit_date', right_on='calendar_date')
+    # combine both datasets to add columns 'day_of_week' and 'holiday_flg' to df
+    df = pd.merge(df, date_info_df, left_on='visit_date', right_on='calendar_date', how='left')
     del df['calendar_date']
     
     if (with_tqdm): 
@@ -121,34 +123,7 @@ def load_from(pwd):
     ### 4. Cleaning Data
     ###
 
-    # 4.7.1 NaN on string columns
-    df['area_name'].fillna(value='', inplace=True)
-    df['genre_name'].fillna(value='', inplace=True)
-    
-    if (with_tqdm): 
-        pbar.update(1) 
-
-    # 4.7.2 NaN on float columns air_store_id and hpg_store_id
-    df.fillna(pd.concat([ df.hpg_store_id.map(store_ids_df.set_index('hpg_store_id').air_store_id),
-                          df.air_store_id.map(store_ids_df.set_index('air_store_id').hpg_store_id),
-                        ], axis=1, keys=['air_store_id', 'hpg_store_id']), inplace=True)
-
-    df['air_store_id'].fillna(value='', inplace=True)
-    df['hpg_store_id'].fillna(value='', inplace=True)
-    
-    if (with_tqdm): 
-        pbar.update(1) 
-
-    # 4.7.3 NaN on numeric columns
-    df['latitude'].fillna(value=-1, inplace=True)
-    df['longitude'].fillna(value=-1, inplace=True)
-    
-    if (with_tqdm): 
-        pbar.update(1) 
-
-    # 4.7.4 NaN on datetime columns
-    df['reserve_datetime'].fillna(value=-9999, inplace=True)
-    df['visit_datetime'].fillna(value=-9999, inplace=True)
+    # TODO handle NaN on reservations
     
     if (with_tqdm): 
         pbar.update(1) 
